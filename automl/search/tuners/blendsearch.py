6c417bb9adb5c0b17431ecbfb77d9c09f733b4a1
Antoni Baum <antoni.baum@protonmail.com>
2021-05-01 21:30:24 +0200

    FLAML update

--- automl/search/tuners/blendsearch.py
+++ automl/search/tuners/blendsearch.py
@@ -322,10 +322,12 @@ class PatchedFLOW2(FLOW2):
             for i, key in enumerate(self._tunable_keys):
                 move[key] -= self._direction_tried[i]
             self._direction_tried = None
-        # propose a new direction
-        self._direction_tried = self.rand_vector_unit_sphere(self.dim) * self.step
-        for i, key in enumerate(self._tunable_keys):
-            move[key] += self._direction_tried[i]
+        else:
+            # propose a new direction
+            self._direction_tried = self.rand_vector_unit_sphere(
+                self.dim) * self.step
+            for i, key in enumerate(self._tunable_keys):
+                move[key] += self._direction_tried[i]
         self._project(move)
         config = self.denormalize(move)
         self._proposed_by[trial_id] = self.incumbent
