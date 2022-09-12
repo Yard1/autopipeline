4a063ad1c21cbe2ea3d693956637cb258bb4633c
Antoni Baum <antoni.baum@protonmail.com>
2022-09-12 12:08:07 +0000

    fix

--- automl/components/estimators/linear_model/__init__.py
+++ automl/components/estimators/linear_model/__init__.py
@@ -2,3 +2,4 @@ from .logistic_regression import LogisticRegression, LogisticRegressionCV
 from .linear_regression import LinearRegression, ElasticNet, ElasticNetCV
 from .svm import LinearSVC, LinearSVR
 from .naive_bayes import GaussianNB
+from .qda import QuadraticDiscriminantAnalysis
